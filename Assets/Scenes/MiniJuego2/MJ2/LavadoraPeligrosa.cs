f9b6a773b97ffba2fec74599f57e60fccbcdab83
Lya <A01424783@tec.mx>
2026-05-28 02:19:47 -0600

    Termine sonido
    Ya le puse sonido a todo mi juego, a ver si jala

--- Assets/Scenes/MiniJuego2/MJ2/LavadoraPeligrosa.cs
+++ Assets/Scenes/MiniJuego2/MJ2/LavadoraPeligrosa.cs
@@ -1,13 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class LavadoraPeligrosa : MonoBehaviour
 {
+    private bool yaTocoJugador = false;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && yaTocoJugador == false)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            yaTocoJugador = true;
+
+            StartCoroutine(ReiniciarConSonido());
         }
+    }
+
+    IEnumerator ReiniciarConSonido()
+    {
+        if (AudioManager.instancia != null)
+        {
+            AudioManager.instancia.ReproducirLavadoraRoja();
+        }
+
+        yield return new WaitForSeconds(0.7f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
