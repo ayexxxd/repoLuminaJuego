ff162df45aa63e02522dabf2453d1b454b3664a1
betsnb <A01413243@tec.mx>
2026-05-27 23:34:33 -0600

    Menu ya bien hecho
    bien hecho es un decir, esta todo feo pero pues ya sirven los botones

--- Assets/Scenes/Menu/menu.cs
+++ Assets/Scenes/Menu/menu.cs
@@ -3,19 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    [Header("Scene Names (must exist in Build Settings)")]
-    [SerializeField] private string miniJuego5StartScene = "StartScene";
-    [SerializeField] private string miniJuego4StartScene = "ExInGameScene";
 
     // Button function: opens MiniJuego5 start scene
     public void OpenMiniJuego5Start()
     {
-        SceneManager.LoadScene(miniJuego5StartScene);
+        SceneManager.LoadScene("StartScene");
     }
 
     // Button function: opens MiniJuego4 start scene
     public void OpenMiniJuego4Start()
     {
         SceneManager.LoadScene("ExInGameScene");
+    }
+
+    public void OpenMiniJuego1Start()
+    {
+        SceneManager.LoadScene("MenuMJ1Escena");
+    }
+
+    public void OpenMiniJuego2Start()
+    {
+        SceneManager.LoadScene("EMI");
+    }
+
+    public void OpenMiniJuego3Start()
+    {
+        SceneManager.LoadScene("SceneCar");
     }
 }
